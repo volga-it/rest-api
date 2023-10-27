696cc53b2fe747d1b8c84cf8c220f8b05a2947bd
Eugene <genyamateyuk@gmail.com>
2023-10-27 12:38:51 +0300

    hidden banned tokens api

--- src/main/java/org/jeugenedev/simbir/repository/BannedTokenRepository.java
+++ src/main/java/org/jeugenedev/simbir/repository/BannedTokenRepository.java
@@ -2,10 +2,12 @@ package org.jeugenedev.simbir.repository;
 
 import org.jeugenedev.simbir.entity.BannedToken;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.sql.Timestamp;
 import java.util.UUID;
 
+@RestResource(exported = false)
 public interface BannedTokenRepository extends JpaRepository<BannedToken, UUID> {
     void deleteByTimeLessThan(Timestamp time);
     boolean existsByTokenBase64Payload(String payload);
